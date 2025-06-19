fff4952b469ba9080c7d8892ad22a61c73bc16e4
Hara Keisuke <harakeisuke7@gmail.com>
2025-06-19 21:44:56 +0900

    PWA対応

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,47 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'  
+import { VitePWA } from 'vite-plugin-pwa'　 
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/kakezan/',
-  plugins: [vue()],
-   resolve: {
+  plugins: [
+    vue(),
+   VitePWA({
+      registerType: 'autoUpdate', // 更新があった場合に自動でリロードする
+      devOptions: {
+        enabled: true // 開発モードでもPWAの動作確認を可能にする
+      },
+      manifest: {
+        name: '２桁掛算', // アプリのフルネーム
+        short_name: '２桁掛算', // ホーム画面に表示される短い名前
+        description: '２桁の掛け算練習ゲームです。', // アプリの説明
+        start_url: '.', // アプリ起動時のURL
+        display: 'standalone', // アドレスバーなどを表示しないネイティブアプリのような表示
+        background_color: '#ffffff', // スプラッシュ画面の背景色
+        theme_color: '#007acc',      // ツールバーの色
+        icons: [
+          {
+            src: 'icon-192x192.png', // publicディレクトリからの相対パス
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png', // publicディレクトリからの相対パス
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  // ビルド時に'@'を'src'として解決するための設定
+  resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+
 
 })
